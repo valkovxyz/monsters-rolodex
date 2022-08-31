50965e052889725160907e526bde03b2f3beec82
Artem Valkov <valkov.artyom.fs@gmail.com>
2022-09-01 01:18:22 +0300

    componentDidMount fetch

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
 import './App.css';
-import {Component} from "react";
+import { Component } from "react";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Linda',
-          id: '123e',
-        },
-        {
-          name: 'Frank',
-          id: '123r',
-        },
-        {
-          name: 'Jacky',
-          id: '123t',
-        },
-        {
-          name: 'Andy',
-          id: '123y'
-        }
-      ]
+      monsters: []
     }
   };
 
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => (response.json()))
+      .then((users) => this.setState(() => {
+        return { monsters: users }
+      }))
+  }
+
   render() {
+    console.log('render')
     return (
       <div className="App">
         {
           this.state.monsters.map((monster) => {
               return (
-                <div key={monster.id}>
-                  <h1>{monster.name}</h1>
+                <div key={ monster.id }>
+                  <h1>{ monster.name }</h1>
                 </div>
               )
             }
