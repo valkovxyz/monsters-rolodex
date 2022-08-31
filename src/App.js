42df252c68970568c35d229987a3ed546b79d32e
Artem Valkov <valkov.artyom.fs@gmail.com>
2022-09-01 00:46:39 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -2,47 +2,47 @@ import './App.css';
 import {Component} from "react";
 
 class App extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            monsters: [
-                {
-                    name: 'Linda',
-                    id: '123e',
-                },
-                {
-                    name: 'Frank',
-                    id: '123r',
-                },
-                {
-                    name: 'Jacky',
-                    id: '123t',
-                },
-                {
-                    name: 'Andy',
-                    id: '123y'
-                }
-            ]
+    this.state = {
+      monsters: [
+        {
+          name: 'Linda',
+          id: '123e',
+        },
+        {
+          name: 'Frank',
+          id: '123r',
+        },
+        {
+          name: 'Jacky',
+          id: '123t',
+        },
+        {
+          name: 'Andy',
+          id: '123y'
         }
-    };
-
-    render() {
-        return (
-            <div className="App">
-                {
-                    this.state.monsters.map((monster) => {
-                            return (
-                                <div key={monster.id}>
-                                    <h1 >{monster.name}</h1>
-                                </div>
-                            )
-                        }
-                    )
-                }
-            </div>
-        );
+      ]
     }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map((monster) => {
+              return (
+                <div key={monster.id}>
+                  <h1>{monster.name}</h1>
+                </div>
+              )
+            }
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
