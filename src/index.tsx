68cdd502291dce525e740076df0fc8a480061a5d
Artem Valkov <valkov.artyom.fs@gmail.com>
2022-11-23 01:31:28 +0200

    refactored to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById('root')
+const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container)
 root.render(
   <React.StrictMode>
@@ -15,4 +14,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
